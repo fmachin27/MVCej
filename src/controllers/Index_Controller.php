bdb23e42e2b85b2d65f46bffd5865c4a181f8677
franco <izipiziwizi@gmail.com>
2023-06-26 19:08:04 -0300

    soluciones en vista

--- src/controllers/Index_Controller.php
+++ src/controllers/Index_Controller.php
@@ -1,13 +1,15 @@
 <?php
-namespace Franco\App\controllers;
+
+
+
 use Franco\App\libs\Controlador;
 
-class Index_Controller extends Controlador {
-   
+class Index_Controller extends Controlador
+{
+
   public function index()
   {
-  
-$this->cargarVista("index/index","datoDemo");
-}
-   }
 
+    $this->cargarVista("index/index", "datoDemo");
+  }
+}
